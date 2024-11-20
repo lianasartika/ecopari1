7454fdd65ae601d12774737ffbdcec74d675e377
Lio wilianto <lianasartika48@upi.edu>
2024-11-20 17:42:04 +0700

    make it better

--- app.js
+++ app.js
@@ -22,6 +22,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+//-momery unleaked---------
+app.set('trust proxy', 1);
+
+app.use(session({
+cookie:{
+    secure: true,
+    maxAge:60000
+       },
+store: new RedisStore(),
+secret: 'secret',
+saveUninitialized: true,
+resave: false
+}));
+
+app.use(function(req,res,next){
+if(!req.session){
+    return next(new Error('Oh no')) //handle error
+}
+next() //otherwise continue
+});
+
 app.use(session({
     secret: 'secret',
     resave: false,
